468cb868fa3c004fdfcef888f2ee0c20fc7f2b07
Linas Jonika <linasjonika@Linass-MacBook-Air.local>
2021-08-10 10:20:22 +0300

    final commit

--- client/src/pages/Dashboard/components/Tasks.js
+++ client/src/pages/Dashboard/components/Tasks.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 export default function Tasks() {
-    const [tasks, setTasks] = useState()
-
+    const [tasks, setTasks] = useState([])
+    useEffect(() => fetchTodos(), [])
+    const fetchTodos = () => {
+        axios({
+            url: '/api/todos',
+            method: 'GET',
+            headers: {
+                ["x-access-token"]: localStorage.getItem('x-access-token')
+            }
+        })
+            .then((response) => {
+                console.log('response:', response.data)
+                setTasks(response.data)
+            })
+            .catch((error) => {
+                console.log(error, 'Not logged in to get tasks')
+            })
+    }
     return (
         <div>
-            {!tasks && <div>Click here to create your task list!</div>}
+            {tasks.length === 0 && <div>Click here to create your task list!</div>}
+            {tasks.length > 0 &&
+                <div><span>{tasks[0].title}</span><input type="checkbox" checked={tasks[0].is_completed} /></div>
+            }
+            {tasks.length > 1 &&
+                <div><span>{tasks[1].title}</span><input type="checkbox" checked={tasks[1].is_completed} /></div>
+            }
+            {tasks.length > 2 &&
+                <div>
+                    <span>{tasks[2].title}</span><input type="checkbox" checked={tasks[2].is_completed} /></div>
+            }
         </div>
     )
 }
