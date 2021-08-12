3a35924a27fb8f8cbda1b7b52cd97f02e757a3ba
Linas Jonika <linasjonika@Linass-MacBook-Air.local>
2021-08-12 16:47:31 +0300

    changed task button

--- client/src/pages/TasksPage/components/RemoveTaskButton.js
+++ client/src/pages/TasksPage/components/RemoveTaskButton.js
@@ -5,7 +5,7 @@ export default function RemoveTaskButton({onClick}) {
     return (
         <div className="remove-task-container">
             <button className="remove-task-button" onClick={onClick}>
-                Remove task
+                X
             </button>
         </div>
     )
